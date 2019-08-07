15f0be62fe5bb87211c34fefa905e8865b41b230
Tore Aurstad <tore.aurstad@hemit.no>
2019-08-07 22:50:22 +0200

    Completed module 2

--- src/app/event-app.component.ts
+++ src/app/event-app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-event-root',
-  template: '<marquee>rulez</marquee><br /><blink>Angular rocks</blink>',
+  template: '<p>Hello World</p>',
   styleUrls: ['./event-app.component.css']
 })
 export class EventAppComponent {
