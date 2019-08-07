d1282e8a034f8a84a4aa232a20ff1ab3583482cb
Tore Aurstad <tore.aurstad@hemit.no>
2019-08-08 01:08:04 +0200

    Completed module 4

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -6,19 +6,29 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   .thumbnail { min-height: 210px; }
   .pad-left { margin-left: 10px; }
   .well div { color: #bbb; }
+  .green { color: #003300 !important; }
+  .bold { font-weight: bold; }
   `],
   template: `
   <div class="well hoverwell thumbnail">
   <h2>{{event.name}}</h2>
   <div>Date: {{event.date}}</div>
-  <div>Time: {{event.time}}</div>
+  <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+   Time: {{event.time}}
+   <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+   <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+   <span *ngSwitchDefault>(Normal Start)</span>
+  </div>
   <div>Price: \${{event.price}}</div>
-   <div>
+   <div [hidden]="!event?.location">
    <span>Address: {{event.location.address}}</span>&nbsp;
    <span class="pad-left">City: {{event.location.city}}</span>&nbsp;
    <span>Country: {{event.location.country}}</span>
    </div>
-  </div>
+   <div *ngIf="event?.onlineUrl">
+    <span>Online url: {{event.onlineUrl }}</span>
+   </div>
+
   <!-- <button class="btn btn-primary" (click)="handleClick($event)">Click me</button> -->`
 })
 export class EventThumbnailComponent {
@@ -32,6 +42,15 @@ export class EventThumbnailComponent {
 
   logFoo() {
    console.log('foo');
+  }
+
+  getStartTimeClass() {
+    // const isEarlyStart = this.event && this.event.time === '8:00 am';
+    // return { green: isEarlyStart, bold: isEarlyStart };
+    if ( this.event && this.event.time === '8:00 am') {
+      return ['green', 'bold'];
+    }
+    return [];
   }
 
 }
