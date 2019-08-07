a82a9902f1e651f9b1f2375a62a553488a43c752
Tore Aurstad <tore.aurstad@hemit.no>
2019-08-07 23:19:30 +0200

    Subcompponent in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { EventsListComponent } from './events/events-list.component';
+import { EventThumbnailComponent } from './events/events-thumbnail.component.ts';
 import { EventAppComponent } from './event-app.component';
 
 @NgModule({
   declarations: [
     EventAppComponent,
-    EventsListComponent
+    EventsListComponent,
+    EventThumbnailComponent
   ],
   schemas: [
     NO_ERRORS_SCHEMA
---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-list',
+  templateUrl: './events-list.component.html'
+})
+export class EventsListComponent {
+
+  event1 = {
+    id: 1,
+    name: 'Angular Connect',
+    date: '19/10/18',
+    time: '10:00am',
+    price: 599.99,
+    location: {
+      address: '1057 DT',
+      city: 'London',
+      country: 'UK'
+    }
+  };
+
+}
