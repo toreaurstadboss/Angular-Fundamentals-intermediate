c274aac36704f87c51f8291e8f94174209d009f4
Tore Aurstad <tore.aurstad@hemit.no>
2019-08-09 13:33:26 +0200

    Compilation error fixes

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
+import { ISession } from "src/app/events";
 
 @Component({
   selector: 'session-list',
   templateUrl: './session-list.component.html'
 })
 export class SessionListComponent {
-  @Input() sessions: ISession[];
+  @Input() sessions: ISession[]; //importing ISession here
 
 }
